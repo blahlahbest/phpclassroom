e705089ca80fcce583b248c933c10324a5e25fe3
blahlahbest <89377708+blahlahbest@users.noreply.github.com>
2021-11-19 14:31:01 +0700

    Update tableloop.php

--- tableloop.php
+++ tableloop.php
@@ -4,27 +4,7 @@
   <body>
     <table border="1">
       <?php
-      for($i=0;$i<5;$i++){
-          echo "<tr>";
-          echo "<td>คอลัมน์</td>";
-          echo "</tr>";
-      for($i=0;$i<5;$i++){
-          echo "<tr>";
-          echo "<td>คอลัมน์</td>";
-          echo "</tr>";
-      for($i=0;$i<5;$i++){
-          echo "<tr>";
-          echo "<td>คอลัมน์</td>";
-          echo "</tr>";
-      for($i=0;$i<5;$i++){
-          echo "<tr>";
-          echo "<td>คอลัมน์</td>";
-          echo "</tr>";
-      for($i=0;$i<5;$i++){
-          echo "<tr>";
-          echo "<td>คอลัมน์</td>";
-          echo "</tr>";
-      for($i=0;$i<5;$i++){
+        for($i=0;$i<5;$i++){
           echo "<tr>";
           echo "<td>คอลัมน์</td>";
           echo "</tr>";
