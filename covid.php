9c68805c613c5e73682b9d9f49d70b8905e0f906
blahlahbest <89377708+blahlahbest@users.noreply.github.com>
2021-11-26 16:01:01 +0700

    Update covid.php

--- covid.php
+++ covid.php
@@ -1,22 +1,19 @@
+Chanadet Kulkrom
 <?php
-  $json = file_get_contents('https://covid19.ddc.moph.go.th/api/Cases/today-cases-all');
+  $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all");
   $data = json_decode($json);
 
   echo "<table border='1'>";
 
-  foreach ( $data[0] as $key => $val ){
-        echo "<tr>";
-        echo "<td>";
-        echo $key;
-        echo "</td>";
-        echo "</tr>";
-  echo "<table border='1'>";
-  foreach ( $data[0] as $key => $val ){
-        echo "<tr>";
-        echo "<td>";
-        echo $key;
-        echo "</td>";
-        echo "</tr>";
+ foreach ( $data[0] as $key => $val ){
+    echo "<tr>";
+    echo "<td>";
+    echo $val."<br>"; 
+    echo "</td>";
+    echo "<td>";
+    echo $key;
+    echo "</td>";
+    echo "</tr>";
   }
 
   echo "</table>";
