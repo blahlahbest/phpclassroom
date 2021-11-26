0b4abf3a6c082a1b4f09438d09f701832597dff1
blahlahbest <89377708+blahlahbest@users.noreply.github.com>
2021-11-26 15:35:49 +0700

    Update covid.php

--- covid.php
+++ covid.php
@@ -3,7 +3,16 @@
   $data = json_decode($json);
 
   echo "<table border='1'>";
-
+  <?php
+      for($i=0;$i<10;$i++){
+          echo "<tr>";
+          echo "<td>txn_date1</td>";
+          echo "<td>new_case</td>";
+          echo "<td>total_case</td>";
+          echo "<td>new_death</td>";
+          echo "<td>total_death</td>";
+          
+          echo "</tr>";
   foreach ( $data[0] as $key => $val ){
     echo $key.":".$val."<br>";
   }
