4b3945229095f1d388948a19de88612ca1ec0102
blahlahbest <89377708+blahlahbest@users.noreply.github.com>
2021-11-26 14:40:02 +0700

    Update covid.php

--- covid.php
+++ covid.php
@@ -6,6 +6,11 @@
 
   foreach ( $data[0] as $key => $val ){
     echo $key.":".$val."<br>";
+    <?php
+      for($i=0;$i<10;$i++){
+          echo "<tr>";
+          echo "<td>คอลัมน์1</td>";
+          echo "<td>คอลัมน์2</td>";
   }
 
   echo "</table>";
