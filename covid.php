9d5e7b645f793329b0ff2de01e5b5ba3a32e5384
blahlahbest <89377708+blahlahbest@users.noreply.github.com>
2021-11-26 15:13:55 +0700

    Update covid.php

--- covid.php
+++ covid.php
@@ -3,17 +3,23 @@
   $data = json_decode($json);
 
   echo "<table border='1'>";
- <?php
+ <html>
+  <head>
+  </head>
+  <body>
+    <table border="1">
+       <?php
       for($i=0;$i<100;$i++){
           echo "<tr>";
-          echo "<td>คอลัมน์1</td>";
-          echo "<td>คอลัมน์2</td>";
-          echo "<td>คอลัมน์3</td>";
-          echo "<td>คอลัมน์4</td>";
-          echo "<td>คอลัมน์5</td>";
-          echo "<td>คอลัมน์6</td>";
+          echo "<td>json_decode($json)</td>";
+          echo "<td>json_decode($json)</td>"
           
           echo "</tr>";
+        }
+       ?>
+    </table>
+  </body>
+</html>
   foreach ( $data[0] as $key => $val ){
     echo $key.":".$val."<br>";
   }
