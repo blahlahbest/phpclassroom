c53d98cbe2159f47d97a6bcbbf9903210d98ab66
blahlahbest <89377708+blahlahbest@users.noreply.github.com>
2022-02-07 21:20:32 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,29 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: myFont;
+        src: url(SanamDeklen_chaya.woff);
+      }
+      @font-face{
+        font-family: myFont2;
+        src: url(Crispy%20Tofu.woff);
+      }
+      #font1{
+        font-family: myFont;
+      }
+      #font2{
+        font-family: myFont2;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="font1">
+      <h1>นางสาวปัณฑิตา จันทร์อนุรักษ์ เลขที่ 22 ห้อง 5/13</h1>
+    </div>
+    <div id="font2">
+      <h1>นางสาวปัณฑิตา จันทร์อนุรักษ์ เลขที่ 22 ห้อง 5/13</h1>
+    </div>
+  </body>
+</html>
