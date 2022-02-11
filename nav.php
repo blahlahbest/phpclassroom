6dd7a6591d312f425bc4e24521e1e015377a85c2
blahlahbest <89377708+blahlahbest@users.noreply.github.com>
2022-02-11 14:06:11 +0700

    Update nav.php

--- nav.php
+++ nav.php
@@ -2,25 +2,26 @@
   <head>
     <title>Navigation Bar</title>
     <style>
-    ul {
-      list-style-type: none;
-    }
-    li a {
-      display: block;
-      padding: 8px 16px;
-      text-decoration: none;
-    }
-    li a:hover {
-      background-color: green;
-      color: white;
-    }
-   </style>
+      ul {
+        list-style-type: none;
+      }
+      li a {
+        display: block;
+        padding: 8px 16px;
+        text-decoration: none;
+        background-color: pink;
+      }
+      li a:hover {
+        background-color: #555;
+        color: white;
+      }
+    </style>
   </head>
   <body>
     <ul>
-      <li><a href="#">Coffee</a></li>
-      <li><a href="#">Tea</a></li>
-      <li><a href="#">Milk</a></li>
+      <li><a href="#">Coffee</a></li> 
+      <li><a href="#">Tea</a></li> 
+      <li><a href="#">Milk</a></li> 
     </ul>
   </body>
 </html>
